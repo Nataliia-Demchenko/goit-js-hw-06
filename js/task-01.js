d3abfccd1db37b6efd998dcc098f497043bd2987
Nataliia-Demchenko <natalidem1986@gmail.com>
2023-06-23 20:19:45 +0200

    Update task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,9 @@
 const numberOfCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${numberOfCategories.length}`);
 
-numberOfCategories.forEach(item => {
-  const categoryText = item.querySelector('h2').textContent;
-  console.log(`Category: ${categoryText}`);
-  
-  const quantityOfElements = item.querySelectorAll('li').length;
-  console.log(`Elements: ${quantityOfElements}`);
-});
+numberOfCategories.forEach((element) =>
+  console.log(
+    `Category: ${element.firstElementChild.textContent}`,
+    `Elements: ${element.lastElementChild.children.length}`
+  )
+);
